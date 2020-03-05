4b5b05251899e0a6b8717d75ccc08e4fe85cc8db
fnaif1998 <59786512+fnaif1998@users.noreply.github.com>
2020-03-05 00:04:35 -0600

    function prototypes declaration
    function prototypes basic format

--- array_utils.h
+++ array_utils.h
@@ -10,3 +10,4 @@ int containsWithin(const int *arr, int size, int x, int i, int j);
 int *paddedCopy(const int *arr, int oldSize, int newSize);
 void reverse(int *arr, int size);
 int *reverseCopy(const int *arr, int size);
+//We can add extra prototypes for each function if we needed to.
