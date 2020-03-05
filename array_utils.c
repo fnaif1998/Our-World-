2ede235d92bc852acb5f0a64d70e4b00aaeb782d
fnaif1998 <59786512+fnaif1998@users.noreply.github.com>
2020-03-04 23:54:20 -0600

    Add files via upload

---
+++ array_utils.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "math.h"
+#include "array_utils.h"
+
+//Function Definitions
+int contains(const int *arr, int size, int x){}
+
+
+
+
+
+int containsWithin(const int *arr, int size, int x, int i, int j){}
+
+
+
+
+
+int *paddedCopy(const int *arr, int oldSize, int newSize){}
+
+
+
+
+
+void reverse(int *arr, int size){}
+
+
+
+
+
+int *reverseCopy(const int *arr, int size){}
---
+++ array_utils.h
@@ -0,0 +1,12 @@
+/*
+Programmer name: Fahad Naif & Tai
+Date: 03-04-2020
+Program Describtion:
+*/
+
+// function prototypes
+int contains(const int *arr, int size, int x);
+int containsWithin(const int *arr, int size, int x, int i, int j);
+int *paddedCopy(const int *arr, int oldSize, int newSize);
+void reverse(int *arr, int size);
+int *reverseCopy(const int *arr, int size);
