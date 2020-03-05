155f10d44a02a21e79b48f65af174a282e54a848
fnaif1998 <59786512+fnaif1998@users.noreply.github.com>
2020-03-05 00:02:23 -0600

    prototypes files
    I created the prototypes files. Those functions were already provided to us in the handout however I believe we can add more extra if we needed to

--- array_utils.h
+++ array_utils.h
@@ -10,3 +10,4 @@ int containsWithin(const int *arr, int size, int x, int i, int j);
 int *paddedCopy(const int *arr, int oldSize, int newSize);
 void reverse(int *arr, int size);
 int *reverseCopy(const int *arr, int size);
+//we can add extra protoypes for each function if we needed the order of the functions prototypes are vital important
